100214b9b456bc37a384c804958a94910e550f08
moujxiaXia <118913922+moujxiaXia@users.noreply.github.com>
2023-03-19 14:40:43 +0800

    Update streamlitchat.py

--- streamlitchat.py
+++ streamlitchat.py
@@ -17,7 +17,7 @@ with st.sidebar:
     txt = st.text_area('Input Chat', '''   ''')
     if st.button('Send'):
         prompt = txt
-        url = 'https://f.openaimouj.uk/v1/completions'
+        url = 'https:///v1/completions'
         # The headers for the API request
         headers = {
         "Content-Type": "application/json",
