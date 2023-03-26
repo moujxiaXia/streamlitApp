d5d2a76d000321335a83025ddfb25b51163b7c83
moujxiaXia <118913922+moujxiaXia@users.noreply.github.com>
2023-03-26 21:42:16 +0800

    Update streamlitchat.py

--- streamlitchat.py
+++ streamlitchat.py
@@ -28,7 +28,7 @@ with st.sidebar:
         "model": "text-davinci-003",
         "prompt": prompt,
         "temperature": 0.7,
-        "max_tokens": 256,
+        "max_tokens": 2048,
         "top_p": 1,
         "frequency_penalty": 0,
         "presence_penalty": 0
